b0b4f71f318f30914083db56a32db3a91f990230
Steve <steveorsino@gmail.com>
2018-12-15 08:10:25 -0500

    Books displaying dynamically from passed props

---
+++ src/Book.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+class Book extends Component {
+    render() {
+        return (
+                    <div className="book">
+                        <div className="book-top">
+                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: this.props.thumbnail }}></div>
+                        <div className="book-shelf-changer">
+                            <select>
+                            <option value="move" disabled>Move to...</option>
+                            <option value="currentlyReading">Currently Reading</option>
+                            <option value="wantToRead">Want to Read</option>
+                            <option value="read">Read</option>
+                            <option value="none">None</option>
+                            </select>
+                        </div>
+                        </div>
+                        <div className="book-title">{this.props.title}</div>
+                        {this.props.authors.map((author) => <div className="book-authors">{author}</div>)}
+                        
+                    </div>
+        );
+    }
+    
+}
+
+export default Book;
